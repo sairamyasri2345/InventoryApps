b76d930ad2088fe4907df06bafdb59740aabce53
sairamyasri <112067104+sairamyasri2345@users.noreply.github.com>
2025-02-14 22:48:22 +0530

    admin-changes

--- adminfront/src/components/pages/notfound/notfound.js
+++ adminfront/src/components/pages/notfound/notfound.js
@@ -1,18 +1,22 @@
-import React, { useState, useEffect, useRef } from "react";
-
+import React from "react";
+import "./notfound.css";
 
 const NotFound = () => {
-    return(
-     <div className="container-fluid">
-        <div className="row">
-            <div className="col-12 col-md-12">
-<div className="d-flex justify-content-center align-items-center" >
-<img  src={require("../../assets/")} alt="notfound-img"/>
-</div>
-            </div>
+  return (
+    <div className="container-fluid m-0 p-0 img-cont">
+      <div className="row vh-100 vw-100 m-0 p-0">
+        <div className="col-12 col-md-12 p-0">
+          <div className="d-flex justify-content-center align-items-center">
+            <img
+              src={require("../../assets/gst_bg_054_19.jpg")}
+              alt="notfound-img"
+              className="notfound-img img-fluid"
+            />
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-     </div>
-    )
-}
 export default NotFound;
